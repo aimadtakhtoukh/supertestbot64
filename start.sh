df98671519fe0a84a5ddf731ea69c0c33d1fe09b
Aimad <aimad.takhtoukh@gmail.com>
2017-12-30 15:47:34 +0100

    Token passé par variable d'environnement

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ mvn clean package
 docker stop testbot
 docker rm testbot
 docker build -t testbot .
-docker run -d --name testbot --restart=always testbot
+docker run -d --name testbot --env-file ./env.list --restart=always testbot
